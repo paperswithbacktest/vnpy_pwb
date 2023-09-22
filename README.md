170747ef3c26b00d276b7d21f4257c0e45074053
edarchimbaud <edouard.darchimbaud@gmail.com>
2023-09-22 23:42:52 +0200

    feat: first version fully implemented

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# vnpy_edarchimbaud
+# EdArchimbaud Data Service Interface for the VeighNa Framework
+
+<p align="center">
+  <img src ="https://vnpy.oss-cn-shanghai.aliyuncs.com/vnpy-logo.png"/>
+</p>
+
+<p align="center">
+    <img src ="https://img.shields.io/badge/version-2.10.15.3-blueviolet.svg"/>
+    <img src ="https://img.shields.io/badge/platform-windows|linux|macos-yellow.svg"/>
+    <img src ="https://img.shields.io/badge/python-3.7|3.8|3.9|3.10-blue.svg" />
+    <img src ="https://img.shields.io/github/license/vnpy/vnpy.svg?color=orange"/>
+</p>
+
+## Description
+
+It supports the following US financial market bar data:
+
+* Stock and ETF options:
+  * NASDAQ: Nasdaq Stock Exchange
+
+
+## Installation
+
+The installation environment is recommended to be based on [[**VeighNa Studio**](https://edarchimbaud.com/veighna-website)] above version 3.6.0.
+
+Use pip command directly:
+
+```bash
+pip install vnpy_edarchimbaud
+```
+
+Or download the source code, unzip it and run it in cmd:
+
+```bash
+pip install .
+```
+
+## Use
+
+You need to fill in the following field information in the global configuration:
+
+| Name              | Meaning  | Required | Examples     |
+|-------------------|----------|----------|--------------|
+| datafeed.name     | Name     | Yes      | edarchimbaud |
+| datafeed.username | Username | No       |              |
+| datafeed.password | Password | No       |              |
---
+++ vnpy_edarchimbaud/edarchimbaud_datafeed.py
@@ -0,0 +1,133 @@
+from datetime import datetime, timedelta
+from typing import Dict, List, Set, Optional, Callable
+
+from datasets import load_dataset
+from numpy import datetime64, ndarray
+from pandas import DataFrame, to_datetime
+
+from vnpy.trader.setting import SETTINGS
+from vnpy.trader.constant import Exchange, Interval
+from vnpy.trader.object import BarData, TickData, HistoryRequest
+from vnpy.trader.utility import round_to, ZoneInfo
+from vnpy.trader.datafeed import BaseDatafeed
+
+
+INTERVAL_VT2EA: Dict[Interval, str] = {
+    Interval.MINUTE: "1mn",
+    Interval.DAILY: "daily",
+}
+
+INTERVAL_ADJUSTMENT_MAP: Dict[Interval, timedelta] = {
+    Interval.MINUTE: timedelta(minutes=1),
+    Interval.DAILY: timedelta(),  # no need to adjust for daily bar
+}
+
+EXCHANGES: Set[Exchange] = {
+    Exchange.NASDAQ,
+}
+
+NYC_TZ = ZoneInfo("America/New_York")
+
+
+class EdarchimbaudDatafeed(BaseDatafeed):
+    """EdArchimbaud data service interface"""
+
+    def __init__(self):
+        """"""
+        self.username: str = SETTINGS["datafeed.username"]
+        self.password: str = SETTINGS["datafeed.password"]
+
+        self.inited: bool = False
+        self.symbols: ndarray = None
+
+    def init(self, output: Callable = print) -> bool:
+        """Initialisation"""
+        if self.inited:
+            return True
+
+        try:
+            dataset = load_dataset("edarchimbaud/perimeter-stocks")
+            df: DataFrame = dataset["train"].to_pandas()
+            self.symbols = df["symbol"].values
+        except Exception as ex:
+            output(f"An unknown exception occurred: {ex}")
+            return False
+
+        self.inited = True
+        return True
+
+    def query_bar_history(
+        self, req: HistoryRequest, output: Callable = print
+    ) -> Optional[List[BarData]]:
+        """Query bar data"""
+        if not self.inited:
+            n: bool = self.init(output)
+            if not n:
+                return []
+
+        symbol: str = req.symbol
+        exchange: Exchange = req.exchange
+        interval: Interval = req.interval
+        start: datetime = req.start
+        end: datetime = req.end
+
+        ea_symbol: str = symbol
+        if symbol not in self.symbols:
+            output(
+                f"EdArchimbaud failed to query bar data: unsupported contract code {req.vt_symbol}"
+            )
+            return []
+
+        ea_interval: str = INTERVAL_VT2EA.get(interval)
+        if not ea_interval:
+            output(
+                f"EdArchimbaud failed to query bar data: unsupported time period {req.interval.value}"
+            )
+            return []
+
+        # In order to convert the basket timestamp (the point at which the bar ends)
+        # to the VeighNa timestamp (the point at which the bar begins)
+        adjustment: timedelta = INTERVAL_ADJUSTMENT_MAP[interval]
+
+        # In order to check the nightly data
+        end += timedelta(1)
+
+        dataset = load_dataset(f"edarchimbaud/timeseries-{ea_interval}-stocks")
+        df: DataFrame = dataset["train"].to_pandas()
+        df["date"] = to_datetime(df["date"])
+        index: ndarray = (
+            (df.symbol == ea_symbol)
+            & (df.date >= datetime64(start))
+            & (df.date <= datetime64(end))
+        )
+        df = df.loc[index, :]
+
+        data: List[BarData] = []
+
+        if df is not None:
+            # Fill NaN to 0
+            df.fillna(0, inplace=True)
+
+            for row in df.itertuples():
+                dt: datetime = row.date.to_pydatetime() - adjustment
+                dt: datetime = dt.replace(tzinfo=NYC_TZ)
+                adj_factor: float = row.adj_close / row.close
+
+                bar: BarData = BarData(
+                    symbol=symbol,
+                    exchange=exchange,
+                    interval=interval,
+                    datetime=dt,
+                    open_price=round_to(row.open * adj_factor, 0.000001),
+                    high_price=round_to(row.high * adj_factor, 0.000001),
+                    low_price=round_to(row.low * adj_factor, 0.000001),
+                    close_price=round_to(row.adj_close, 0.000001),
+                    volume=row.volume,
+                    turnover=row.volume * row.adj_close,
+                    open_interest=0,
+                    gateway_name="EA",
+                )
+
+                data.append(bar)
+
+        return data
